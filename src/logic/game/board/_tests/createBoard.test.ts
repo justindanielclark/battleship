4157836cb7e7f263882e8817173a41a562789739
JClark <justindanielclark@gmail.com>
2023-01-09 11:11:51 -0800

    initial touch - createBoard.test.ts

--- src/logic/game/board/_tests/createBoard.test.ts
+++ src/logic/game/board/_tests/createBoard.test.ts
@@ -1,1 +1,12 @@
 import {Board} from '../board';
+
+const board = new Board(10, 10);
+
+describe('the board created by new Board', ()=>{
+	test('test1', ()=>{
+		expect(1).toBe(1);
+	});
+	test('test2', ()=>{
+		expect(2).toBe(2);
+	});
+});
