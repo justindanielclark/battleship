e18c5269eedd745d4cf23af8309cef3848ded243
JClark <justindanielclark@gmail.com>
2023-01-12 09:57:08 -0800

    Prettier cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,53 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
 
-let mode = 'development';
-if (process.env.NODE_ENV === 'production'){
-	mode = 'production';
+let mode = "development";
+if (process.env.NODE_ENV === "production") {
+  mode = "production";
 }
 module.exports = {
-	devtool: 'source-map',
-	devServer: {
-		static: './dist',
-		hot: true,
-	},
-	entry: './src/index.ts',
-	mode,
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				use: {
-					loader: 'babel-loader',
-				}
-			},
-			{
-        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-        type: 'asset',
+  devtool: "source-map",
+  devServer: {
+    static: "./dist",
+    hot: true,
+  },
+  entry: "./src/index.ts",
+  mode,
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
       },
-			{
-				test: /\.css$/i,
-				use: ['style-loader', 'css-loader', 'postcss-loader'],
-			},
-			{
-				test: /\.tsx?$/,
-				use: 'ts-loader',
-				exclude: /node_modules/,
-			}
-		]
-	},
-	resolve: {
-		extensions: ['.tsx', '.ts', '.js'],
-	},
-	output: {
-		filename: 'bundle.js',
-		path: path.resolve(__dirname, 'dist'),
-	},
-	plugins: [new HtmlWebpackPlugin({
-		title: 'Battleship'
-	})],
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        exclude: /node_modules/,
+        type: "asset",
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Battleship",
+    }),
+  ],
 };
