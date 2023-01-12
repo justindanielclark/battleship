2802d298869300321fe679b5d7c8c070a51ce9bc
JClark <justindanielclark@gmail.com>
2023-01-12 00:38:01 -0800

    added drawBoard and drawMarker methods

--- src/renderer.ts
+++ src/renderer.ts
@@ -1,6 +1,6 @@
 import { CanvasConfig } from "./canvas";
 import { ModelSprites } from "./assets/ModelSprites";
-import { TextSprites } from "./assets/TextSprites";
+import { TextSprites, validTextSpriteVals } from "./assets/TextSprites";
 import Point from "./logic/Point";
 import Board from "./logic/Board";
 
@@ -10,41 +10,126 @@ const renderer = (
   modelSprites: ModelSprites,
   textSprites: TextSprites
 ) => {
-  const _drawerDimensions = {
+  let _fractional = 0;
+  const _drawerView = {
     start: new Point(0, 0),
     end: new Point(0, 0),
+    scale: 0,
   };
-  const _mainDimensions = {
+  const _mainView = {
     start: new Point(0, 0),
     end: new Point(0, 0),
-  };
-  const _boardDimensions = {
-    start: new Point(0, 0),
-    end: new Point(0, 0),
+    scale: 0,
   };
   function render(): void {
     _clearCanvas();
     ctx.imageSmoothingEnabled = false;
     ctx.fillStyle = "rgb(45,45,45)";
     ctx.fillRect(
-      _drawerDimensions.start.x,
-      _drawerDimensions.start.y,
-      _drawerDimensions.end.x,
-      _drawerDimensions.end.y
+      _drawerView.start.x,
+      _drawerView.start.y,
+      _drawerView.end.x,
+      _drawerView.end.y
     );
     ctx.fillStyle = "rgb(35,35,35)";
     ctx.fillRect(
-      _mainDimensions.start.x,
-      _mainDimensions.start.y,
-      _mainDimensions.end.x,
-      _mainDimensions.end.y
+      _mainView.start.x,
+      _mainView.start.y,
+      _mainView.end.x,
+      _mainView.end.y
     );
+    if (modelSprites.loaded && textSprites.loaded) {
+      // _drawDefensiveBoard();
+      _drawOffensiveBoard();
+      _drawTileDesignations();
+    }
   }
-  function _drawDefensiveBoard(board: Board) {
-    //todo
+  function _drawDefensiveBoard() {
+    for (let i = 0; i < 15; i++) {
+      for (let j = 0; j < 15; j++) {
+        _drawSprite(
+          (i + j) % 2 === 0
+            ? modelSprites.waterTiles[0]
+            : modelSprites.waterTiles[1],
+          new Point(
+            _mainView.start.x + _fractional + _fractional * i,
+            _mainView.start.y + _fractional + _fractional * j
+          ),
+          _mainView.scale,
+          0
+        );
+      }
+    }
   }
-  function _drawOffensiveBoard(board: Board) {
-    //todo
+  function _drawTileDesignations() {
+    const Letters: Array<validTextSpriteVals> = [
+      "A",
+      "B",
+      "C",
+      "D",
+      "E",
+      "F",
+      "G",
+      "H",
+      "I",
+      "J",
+      "K",
+      "L",
+      "M",
+      "N",
+      "O",
+    ];
+    const Numbers: Array<validTextSpriteVals> = [
+      "0",
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+    ];
+    for (let i = 0; i < Letters.length; i++) {
+      _drawSprite(
+        textSprites[Letters[i]] as ImageBitmap,
+        new Point(
+          _mainView.start.x,
+          _fractional + _fractional * i + _mainView.start.y
+        ),
+        _mainView.scale * 2,
+        0
+      );
+    }
+    for (let i = 0; i < Numbers.length; i++) {
+      _drawSprite(
+        textSprites[Numbers[i]] as ImageBitmap,
+        new Point(
+          _fractional + _fractional * i + _mainView.start.x,
+          _mainView.start.y
+        ),
+        _mainView.scale * 2,
+        0
+      );
+    }
+  }
+  function _drawOffensiveBoard() {
+    for (let i = 0; i < 15; i++) {
+      for (let j = 0; j < 15; j++) {
+        _drawSprite(
+          (i + j) % 2 === 0
+            ? modelSprites.radarTiles[0]
+            : modelSprites.radarTiles[1],
+          new Point(
+            _mainView.start.x + _fractional + _fractional * i,
+            _mainView.start.y + _fractional + _fractional * j
+          ),
+          _mainView.scale,
+          0
+        );
+      }
+    }
   }
   function _clearCanvas(): void {
     ctx.clearRect(0, 0, canvasData.width, canvasData.height);
@@ -73,31 +158,33 @@ const renderer = (
     ctx.rotate((-angle * Math.PI) / 180);
     ctx.translate(-drawLoc.x - scaledWidth / 2, -drawLoc.y - scaledHeight / 2);
   }
-  function updateDimensions(): void {
+  function updateViewSizes(): void {
     if (canvasData.orientation === "landscape") {
-      _drawerDimensions.start.x = 0;
-      _drawerDimensions.start.y = 0;
-      _drawerDimensions.end.x = canvasData.width / 4;
-      _drawerDimensions.end.y = canvasData.height;
-      _mainDimensions.start.x = canvasData.width / 4;
-      _mainDimensions.start.y = 0;
-      _mainDimensions.end.x = canvasData.width;
-      _mainDimensions.end.y = canvasData.height;
+      _drawerView.start.x = 0;
+      _drawerView.start.y = 0;
+      _drawerView.end.x = canvasData.width / 4;
+      _drawerView.end.y = canvasData.height;
+      _mainView.start.x = canvasData.width / 4;
+      _mainView.start.y = 0;
+      _mainView.end.x = canvasData.width;
+      _mainView.end.y = canvasData.height;
     }
     if (canvasData.orientation === "portrait") {
-      _drawerDimensions.start.x = 0;
-      _drawerDimensions.start.y = 0;
-      _drawerDimensions.end.x = canvasData.width;
-      _drawerDimensions.end.y = canvasData.height / 4;
-      _mainDimensions.start.x = 0;
-      _mainDimensions.start.y = canvasData.height / 4;
-      _mainDimensions.end.x = canvasData.width;
-      _mainDimensions.end.y = canvasData.height;
+      _drawerView.start.x = 0;
+      _drawerView.start.y = 0;
+      _drawerView.end.x = canvasData.width;
+      _drawerView.end.y = canvasData.height / 4;
+      _mainView.start.x = 0;
+      _mainView.start.y = canvasData.height / 4;
+      _mainView.end.x = canvasData.width;
+      _mainView.end.y = canvasData.height;
     }
+    _fractional = (_mainView.end.x - _mainView.start.x) / 16;
+    _mainView.scale = (_mainView.end.x - _mainView.start.x) / 256;
   }
   return {
     render,
-    updateDimensions,
+    updateViewSizes,
   };
 };
 
