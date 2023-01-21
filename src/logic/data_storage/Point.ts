a39c39fb8a98ada28c60a6d24b60b54c3efcb6e3
JClark <justindanielclark@gmail.com>
2023-01-20 22:15:25 -0800

    +toString() Method

--- src/logic/data_storage/Point.ts
+++ src/logic/data_storage/Point.ts
@@ -8,6 +8,17 @@ class Point {
   deepCopy(): Point {
     return new Point(this.x, this.y);
   }
+  isBetween(pointA: Point, pointB: Point): boolean {
+    return (
+      this.x >= pointA.x &&
+      this.x < pointB.x &&
+      this.y >= pointA.y &&
+      this.y < pointB.y
+    );
+  }
+  toString() {
+    return `{x: ${this.x}, y: ${this.y}}`;
+  }
 }
 
 export default Point;
